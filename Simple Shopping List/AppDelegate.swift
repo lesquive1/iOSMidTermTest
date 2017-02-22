e32bfefadbfb902839b30f422bfa4c46124ad23b
Luis Esquivel <laesquivel@hotmail.com>
2017-02-21 22:06:48 -0500

    removing firebase from appdelegate

--- Simple Shopping List/AppDelegate.swift
+++ Simple Shopping List/AppDelegate.swift
@@ -8,21 +8,21 @@
 //
 
 import UIKit
-import Firebase
-import FirebaseDatabase
+//import Firebase
+//import FirebaseDatabase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+/*
     override init() {
         super.init()
         FIRApp.configure()
         
         FIRDatabase.database().persistenceEnabled = true
     }
-
+*/
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
